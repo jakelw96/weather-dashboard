acbc19787a8f40ac84ccc7bf36f91c96e8a16420
jakelw96 <jake.lw96@gmail.com>
2021-04-21 18:55:28 -0600

    Continued work on logic. Working on current weather box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,59 @@
 var $curTemp = document.getElementById("curr-temp");
 var $curWind = document.getElementById("curr-wind");
 var $curHumidity = document.getElementById("curr-humidity");
+var $curUV = document.getElementById("curr-uvIndex");
 var $inputCity = document.getElementById("city-input");
 
-
-var getWeatherData = function(cityName) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=6855f141fbc2348ef120b63c317a3472";
-    
-    // Make a request to url 
-    fetch(apiUrl).then(function(response) {
+// To get coordinates from API
+var getWeatherDataCoord = function(cityName) {
+    var apiUrlCoord = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=6855f141fbc2348ef120b63c317a3472";
+    // Make a request to url to get coordinates
+    fetch(apiUrlCoord).then(function(response) {
         // Request was successful
         if (response.ok) {
             response.json().then(function(data) {
+                // Get latitude and longitude
                 console.log(data)
-                displayWeatherData(data);
+                getWeatherData(data)
+            });
+        };
+    });
+};
+
+// To get weather data from API
+var getWeatherData = function (cityCoord) {
+    var lat = cityCoord.coord.lat;
+    var lon = cityCoord.coord.lon;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&units=imperial&appid=6855f141fbc2348ef120b63c317a3472";
+    // Make fetch request to url for weather data
+    fetch(apiUrl).then(function(response) {
+        // Request was sucessful
+        if (response.ok) {
+            response.json().then(function(data) {
+               console.log(data)
+               displayWeatherData(data)
             });
         };
     });
 };
 
 var displayWeatherData = function(cityInfo) {
-    //To get current temperature 
-    var curTemp = cityInfo.main.temp;
-    // To convert from Kelvin to Farenheit
-    var curTempConverted = parseInt(((curTemp - 273.15)*1.8) + 32);
+    // Variables for current temperature, wind speed, humidity, and UV index 
+    var curTemp = cityInfo.current.temp;
+    var curWind = cityInfo.current.wind_speed;
+    var curHumid = cityInfo.current.humidity;
+    var curUV = cityInfo.current.uvi;
+
+    // Adds a 
+
+    // Add current weather conditions to p elements
+    $($curTemp).text("Temperature: " + parseInt(curTemp) + "\u00B0");
+    $($curWind).text("Wind: " + parseInt(curWind) + " MPH");
+    $($curHumidity).text("Humidity: " + parseInt(curHumid) + " %");
     
-    //Add current temperature to temperature p element
-    $($curTemp).text("Temperature: " + curTempConverted + "\u00B0");
     
 
-    //Append element to current weather div
+    
     
 
         
@@ -37,5 +61,5 @@ var displayWeatherData = function(cityInfo) {
 }
 
 $("#searchBtn").on("click", function() {
-    getWeatherData($inputCity.value);
+    getWeatherDataCoord($inputCity.value);
 });
